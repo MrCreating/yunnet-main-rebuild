2966231eb9fadcfd938b36ca4fa6f7d2759b6044
MrCreating <mrcreating2002@gmail.com>
2022-09-23 18:33:25 +0300

    lint1

--- bin/lang/Language.php
+++ bin/lang/Language.php
@@ -10,6 +10,7 @@ class Language extends BaseObject
     public string $id;
     public string $email;
     public string $password;
+    public string $sign_in;
 
     ///////////////////////////////
     public static function get (): Language
