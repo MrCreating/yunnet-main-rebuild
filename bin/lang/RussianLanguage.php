0f14e04df6489634829032395d81c2a7c769b6a0
MrCreating <mrcreating2002@gmail.com>
2022-09-23 18:28:04 +0300

    lint1

--- bin/lang/RussianLanguage.php
+++ bin/lang/RussianLanguage.php
@@ -7,4 +7,5 @@ class RussianLanguage extends Language
     public string $id = 'ru';
     public string $email = 'Адрес электронной почты';
     public string $password = 'Пароль';
+    public string $sign_in = 'Войти';
 }
