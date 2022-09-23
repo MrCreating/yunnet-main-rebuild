e723bdf8594af0cdd5f0650303df437f3949ee13
MrCreating <mrcreating2002@gmail.com>
2022-09-23 18:14:16 +0300

    lint1

--- bin/lang/EnglishLanguage.php
+++ bin/lang/EnglishLanguage.php
@@ -7,4 +7,5 @@ class EnglishLanguage extends Language
     public string $id = 'en';
     public string $email = 'E-mail';
     public string $password = 'Password';
+    public string $sign_in = 'Sign In';
 }
